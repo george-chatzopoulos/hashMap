8c877bea06be9cc6c4234a489b0f41e7ab6d21d0
George Chatzopoulos <79384530+george-chatzopoulos@users.noreply.github.com>
2024-11-21 22:35:16 +0200

    Remove the added space in the interface signature

--- HashMapInterface.java
+++ HashMapInterface.java
@@ -1,4 +1,4 @@
-public interface  HashMapInterface<K, V> {
+public interface HashMapInterface<K, V> {
     public void put(K key, V value);
     public V get(K key) throws NullPointerException;
     public Node<K, V> remove(K key) throws NullPointerException;
